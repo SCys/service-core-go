91a0ece83ba83344b935b96e9fda25954543a72a
SCys <me@iscys.com>
2021-01-05 22:59:47 +0800

    + rand

---
+++ rand.go
@@ -0,0 +1,53 @@
+package core
+
+import "crypto/rand"
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// SecureRandomString returns a string of the requested length,
+// made from the byte characters provided (only ASCII allowed).
+// Uses crypto/rand for security. Will panic if len(availableCharBytes) > 256.
+func SecureRandomString(length int) string {
+
+	// Compute bitMask
+	availableCharLength := len(letterBytes)
+	if availableCharLength == 0 || availableCharLength > 256 {
+		panic("availableCharBytes length must be greater than 0 and less than or equal to 256")
+	}
+	var bitLength byte
+	var bitMask byte
+	for bits := availableCharLength - 1; bits != 0; {
+		bits = bits >> 1
+		bitLength++
+	}
+	bitMask = 1<<bitLength - 1
+
+	// Compute bufferSize
+	bufferSize := length + length/3
+
+	// Create random string
+	result := make([]byte, length)
+	for i, j, randomBytes := 0, 0, []byte{}; i < length; j++ {
+		if j%bufferSize == 0 {
+			// Random byte buffer is empty, get a new one
+			randomBytes = SecureRandomBytes(bufferSize)
+		}
+		// Mask bytes to get an index into the character slice
+		if idx := int(randomBytes[j%length] & bitMask); idx < availableCharLength {
+			result[i] = letterBytes[idx]
+			i++
+		}
+	}
+
+	return string(result)
+}
+
+// SecureRandomBytes returns the requested number of bytes using crypto/rand
+func SecureRandomBytes(length int) []byte {
+	var randomBytes = make([]byte, length)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		F("Unable to generate random bytes")
+	}
+	return randomBytes
+}
