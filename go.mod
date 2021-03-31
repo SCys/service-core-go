5686eeece585a41235efe716e66efe491755d09d
SCys <me@iscys.com>
2021-03-31 19:43:25 +0800

    * fix PGX update func

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/scys/core
 go 1.16
 
 require (
-	github.com/gofiber/fiber/v2 v2.5.0
-	github.com/jackc/pgx/v4 v4.10.1
+	github.com/gofiber/fiber/v2 v2.7.1
+	github.com/jackc/pgx/v4 v4.11.0
 	github.com/json-iterator/go v1.1.10
-	github.com/phuslu/log v1.0.67
-	github.com/rs/xid v1.2.1
+	github.com/phuslu/log v1.0.70
+	github.com/rs/xid v1.3.0
 	github.com/valyala/fastjson v1.6.3
 )
--- pgx_utils.go
+++ pgx_utils.go
@@ -7,21 +7,19 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-// PGXUpdateHelper simple update query
-func PGXUpdateHelper(ctx context.Context, name string, data H, db *pgxpool.Pool) error {
+// PGXUpdateHelper 快速更新的辅佐
+func PGXUpdateHelper(ctx context.Context, name string, id interface{}, data H, db *pgxpool.Pool) error {
 	tx, err := db.Begin(ctx)
 	if err != nil {
 		return err
 	}
 
-	for key, name := range data {
+	for key, value := range data {
 		query := fmt.Sprintf("update %s set %s=$2 where id=$1", name, key)
-
-		if _, err := tx.Exec(ctx, query, name); err != nil {
+		if _, err := tx.Exec(ctx, query, id, value); err != nil {
 			_ = tx.Rollback(ctx)
 			return err
 		}
-
 	}
 
 	return tx.Commit(ctx)
