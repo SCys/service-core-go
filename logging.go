d9105cbb6809bde06512b0cc23f08d0ec04ac139
SCys <me@iscys.com>
2021-06-22 22:26:20 +0800

    * 更新日志文件的处理方式

--- logging.go
+++ logging.go
@@ -2,37 +2,147 @@ package core
 
 import (
 	"context"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/phuslu/log"
 )
 
 var loggerSupportExtraFields = []string{"client_ip", "_id", "api"}
+var loggerRoot = log.Logger{Level: log.DebugLevel}
+
+type SimpleFileWriter struct {
+	Filename   string
+	Filemode   fs.FileMode
+	MaxSize    int64
+	MaxBackups int
+
+	WrotenSize        int64 // RO 记录写入数量
+	WrotenCurrentSiez int64 // RO 当前文件写入计数器
+	File              *os.File
+	TimeFormat        string
+	Mut               sync.Mutex
+}
+
+func (w *SimpleFileWriter) WriteEntry(e *log.Entry) (n int, err error) {
+	return w.Write(e.Value())
+}
+
+func (w *SimpleFileWriter) Write(p []byte) (n int, err error) {
+	if w.File == nil {
+		return len(p), nil
+	}
+
+	n, err = w.File.Write(p)
+	if err != nil {
+		return
+	}
+
+	w.WrotenSize += int64(n)
+	if w.MaxSize > 0 && w.WrotenSize > w.MaxSize && w.Filename != "" {
+		err = w.Rotate()
+	}
+
+	return
+}
+
+func (w *SimpleFileWriter) Open() (err error) {
+	err = os.MkdirAll(filepath.Dir(w.Filename), 0755)
+	if err == nil {
+		w.File, err = os.OpenFile(w.Filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err == nil {
+			w.Mut = sync.Mutex{}
+		}
+	}
+
+	return
+}
+
+func (w *SimpleFileWriter) Rotate() (err error) {
+	// rename to new filename
+	now := time.Now()
+	prefix := filepath.Base(w.Filename)
+	name := fmt.Sprintf("%s.%s.log", prefix, now.Format(time.RFC3339))
+	os.Rename(w.Filename, name)
+
+	// keep old
+	old := w.File
+
+	f, err := os.OpenFile(w.Filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return
+	}
+	w.File = f
+
+	go func(old *os.File, prefix string) {
+		// change uid/gid
+		uid, _ := strconv.Atoi(os.Getenv("SUDO_UID"))
+		gid, _ := strconv.Atoi(os.Getenv("SUDO_GID"))
+		if uid != 0 && gid != 0 && os.Geteuid() == 0 {
+			// _ = os.Lchown(prefix+".log", uid, gid)
+			_ = os.Chown(prefix+".log", uid, gid)
+		}
+
+		dir := filepath.Dir(w.Filename)
+		dirfile, err := os.Open(dir)
+		if err != nil {
+			return
+		}
+		infos, err := dirfile.Readdir(-1)
+		dirfile.Close()
+		if err != nil {
+			return
+		}
+
+		matches := make([]os.FileInfo, 0)
+		for _, info := range infos {
+			name := info.Name()
+			if strings.HasPrefix(name, prefix) && strings.HasSuffix(name, ".gz") {
+				matches = append(matches, info)
+			}
+		}
+		sort.Slice(matches, func(i, j int) bool {
+			return matches[i].ModTime().Unix() < matches[j].ModTime().Unix()
+		})
+
+		for i := 0; i < len(matches)-w.MaxBackups-1; i++ {
+			os.Remove(filepath.Join(dir, matches[i].Name()))
+		}
+	}(old, prefix)
+
+	return
+}
+
+func NewFileWriter(name string, maxSize int64, maxBackups int) *SimpleFileWriter {
+	w := &SimpleFileWriter{Filename: name, MaxSize: maxSize, MaxBackups: maxBackups}
+
+	if err := w.Open(); err != nil {
+		loggerRoot.Fatal().Msgf("open log failed:%s", err.Error())
+	}
+
+	return w
+}
 
 var loggerInfo = log.Logger{
-	Level: log.InfoLevel,
-	Writer: &log.FileWriter{
-		Filename: "log/info.log", MaxSize: 70 << 20, MaxBackups: 60,
-		ProcessID: false,
-		HostName:  false,
-	},
+	Level:  log.InfoLevel,
+	Writer: NewFileWriter("log/info.log", 70<<20, 60),
 }
 
 var loggerWarn = log.Logger{
-	Level: log.WarnLevel,
-	Writer: &log.FileWriter{
-		Filename: "log/warn.log", MaxSize: 70 << 20, MaxBackups: 60,
-		ProcessID: false,
-		HostName:  false,
-	},
+	Level:  log.WarnLevel,
+	Writer: NewFileWriter("log/warn.log", 70<<20, 60),
 }
 
 var loggerError = log.Logger{
-	Level: log.ErrorLevel,
-	Writer: &log.FileWriter{
-		Filename: "log/error.log", MaxSize: 70 << 20, MaxBackups: 60,
-		ProcessID: false,
-		HostName:  false,
-	},
+	Level:  log.ErrorLevel,
+	Writer: NewFileWriter("log/error.log", 70<<20, 60),
 }
 
 // loggerDebug debug logger
