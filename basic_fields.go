ef538870533fb95eb5697dc346805381026cdfe5
SCys <me@iscys.com>
2022-01-16 01:13:16 +0800

    * update BasicFields

---
+++ basic_fields.go
@@ -0,0 +1,142 @@
+package core
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/rs/xid"
+	"github.com/valyala/fastjson"
+)
+
+var (
+	BasicFieldsInlineFields     = []string{"id", "ts_create", "ts_update", "removed", "info"}
+	BasicFieldsInlineFieldsSize = len(BasicFieldsInlineFields)
+)
+
+// BasicFieldsInterface basic fields interface with dump function
+type BasicFieldsInterface interface {
+	Dump() []byte
+	Name() string
+}
+
+// BasicFields basic fields
+type BasicFields struct {
+	BasicFieldsInterface
+
+	ID       string    `json:"id"`
+	TSCreate time.Time `json:"ts_create"`
+	TSUpdate time.Time `json:"ts_update"`
+	Removed  bool      `json:"removed"`
+	Info     H         `json:"info"`
+}
+
+// NewBasicFields basicFields init
+func NewBasicFields() BasicFields {
+	now := Now()
+
+	return BasicFields{
+		ID:       xid.NewWithTime(now).String(),
+		TSCreate: now,
+		TSUpdate: now,
+		Removed:  false,
+		Info:     H{},
+	}
+}
+
+// LoadBasicFields load with default fields
+func LoadBasicFields(i interface{}, value *fastjson.Value) {
+	x, ok := i.(*BasicFields)
+	if !ok {
+		return
+	}
+
+	x.ID = String(value.GetStringBytes("id"))
+	x.TSCreate, _ = time.Parse(time.RFC3339Nano, String(value.GetStringBytes("ts_create")))
+	x.TSUpdate, _ = time.Parse(time.RFC3339Nano, String(value.GetStringBytes("ts_update")))
+	x.Removed = value.GetBool("removed")
+
+	_ = jsoniter.Unmarshal(value.GetStringBytes("info"), &x.Info)
+}
+
+// PGXInsert 插入
+func (b *BasicFields) PGXInsert(ctx context.Context, db *pgxpool.Pool) (pgx.Rows, error) {
+	table_name := b.Name()
+	fields_elm := reflect.ValueOf(b).Elem()
+	fields_size := fields_elm.NumField()
+
+	values := make([]interface{}, 0, BasicFieldsInlineFieldsSize+fields_size)
+
+	values = append(values, b.ID)
+	values = append(values, b.TSCreate)
+	values = append(values, b.TSUpdate)
+	values = append(values, b.Removed)
+	values = append(values, b.Info)
+
+	arguments := make([]string, 0, fields_size)
+	arguments_q := make([]string, 0, fields_size)
+
+	for i := 0; i < fields_size; i++ {
+		valueField := fields_elm.Field(i)
+		typeField := fields_elm.Type().Field(i)
+		tag := typeField.Tag
+
+		name := tag.Get("json")
+
+		// ignore inline fields
+		if sort.SearchStrings(BasicFieldsInlineFields, name) < BasicFieldsInlineFieldsSize {
+			continue
+		}
+
+		values = append(values, valueField.Interface())
+		arguments = append(arguments, name)
+		arguments_q = append(arguments_q, fmt.Sprintf("$%d", len(values)))
+	}
+
+	return db.Query(ctx, fmt.Sprintf(
+		"insert into %s (id,ts_create,ts_update,removed,info,%s) values($1,$2,$3,$4,$5,%s)",
+		table_name, strings.Join(arguments, ","), strings.Join(arguments_q, ","),
+	), values...)
+}
+
+// PGXUpdate 更新
+func (b *BasicFields) PGXUpdate(ctx context.Context, db *pgxpool.Pool, data H) (pgx.Rows, error) {
+	table_name := b.Name()
+	fields_size := len(data)
+
+	values := make([]interface{}, 0, fields_size)
+	arguments := make([]string, 0, fields_size)
+
+	// loop data
+	for k, v := range data {
+		if k == "id" {
+			continue
+		}
+
+		values = append(values, v)
+		arguments = append(arguments, fmt.Sprintf("%s=$%d", k, len(values)))
+	}
+
+	sql := fmt.Sprintf("update %s set %s where id=$1", table_name, strings.Join(arguments, ","))
+	return db.Query(ctx, sql, values...)
+}
+
+// PGXRemove 删除
+func (b *BasicFields) PGXRemove(ctx context.Context, db *pgxpool.Pool) error {
+	table_name := b.Name()
+
+	_, err := db.Exec(ctx, fmt.Sprintf("update %s set removed=true,ts_update=$2 where id=$1", table_name),
+		b.ID, b.TSUpdate)
+
+	return err
+}
+
+func init() {
+	sort.Strings(BasicFieldsInlineFields)
+}
--- struct.go
+++ struct.go
@@ -1,58 +1,7 @@
 package core
 
-import (
-	"time"
-
-	jsoniter "github.com/json-iterator/go"
-	"github.com/valyala/fastjson"
-
-	"github.com/rs/xid"
-)
-
 // H alias map[string]interface{}
 type H = map[string]interface{}
 
 // HNil empty struct
 var HNil = H{}
-
-// BasicFieldsInterface basic fields interface with dump function
-type BasicFieldsInterface interface {
-	Dump() []byte
-}
-
-// BasicFields basic fields
-type BasicFields struct {
-	ID       string    `json:"id"`
-	TSCreate time.Time `json:"ts_create"`
-	TSUpdate time.Time `json:"ts_update"`
-	Removed  bool      `json:"removed"`
-	Info     H         `json:"info"`
-}
-
-// NewBasicFields basicFields init
-func NewBasicFields() BasicFields {
-	now := Now()
-
-	return BasicFields{
-		ID:       xid.NewWithTime(now).String(),
-		TSCreate: now,
-		TSUpdate: now,
-		Removed:  false,
-		Info:     H{},
-	}
-}
-
-// LoadBasicFields load with default fields
-func LoadBasicFields(i interface{}, value *fastjson.Value) {
-	x, ok := i.(*BasicFields)
-	if !ok {
-		return
-	}
-
-	x.ID = String(value.GetStringBytes("id"))
-	x.TSCreate, _ = time.Parse(time.RFC3339Nano, String(value.GetStringBytes("ts_create")))
-	x.TSUpdate, _ = time.Parse(time.RFC3339Nano, String(value.GetStringBytes("ts_update")))
-	x.Removed = value.GetBool("removed")
-
-	_ = jsoniter.Unmarshal(value.GetStringBytes("info"), &x.Info)
-}
