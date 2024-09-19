4c57ea6a1eec97c05b8f25b5afa050d3a905a0b6
SCys <me@iscys.com>
2024-09-19 13:17:21 +0800

    * change logging use golib fileWriter

--- go.mod
+++ go.mod
@@ -1,15 +1,15 @@
 module github.com/scys/service-core-go
 
-go 1.18
+go 1.23
 
 require (
-	github.com/bytedance/sonic v1.11.8
-	github.com/gofiber/fiber/v2 v2.52.4
+	github.com/bytedance/sonic v1.12.2
+	github.com/gofiber/fiber/v2 v2.52.5
 	github.com/gojek/heimdall/v7 v7.0.3
-	github.com/jackc/pgx/v5 v5.6.0
+	github.com/jackc/pgx/v5 v5.7.1
 	github.com/mattn/go-sqlite3 v1.14.12
-	github.com/phuslu/log v1.0.104
-	github.com/rs/xid v1.5.0
+	github.com/phuslu/log v1.0.111
+	github.com/rs/xid v1.6.0
 	github.com/stretchr/testify v1.9.0
 	github.com/valyala/bytebufferpool v1.0.0
 	github.com/valyala/fastjson v1.6.4
@@ -18,7 +18,7 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/bytedance/sonic/loader v0.2.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -26,25 +26,25 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/puddle/v2 v2.2.1 // indirect
-	github.com/klauspost/compress v1.17.8 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
 	github.com/kr/text v0.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
-	github.com/valyala/fasthttp v1.54.0 // indirect
+	github.com/valyala/fasthttp v1.55.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.24.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/arch v0.10.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- logging.go
+++ logging.go
@@ -2,145 +2,44 @@ package core
 
 import (
 	"context"
-	"io/fs"
-	"os"
-	"path/filepath"
-	"sort"
-	"strconv"
 	"strings"
-	"sync"
-	"time"
 
 	"github.com/phuslu/log"
 )
 
 var loggerSupportExtraFields = []string{"client_ip", "_id"}
-var loggerRoot = log.Logger{Level: log.DebugLevel}
-
-type SimpleFileWriter struct {
-	Filename   string
-	Filemode   fs.FileMode
-	MaxSize    int64
-	MaxBackups int
-
-	Written    int64 // RO 记录写入数量
-	File       *os.File
-	TimeFormat string
-	Mut        sync.Mutex
-}
-
-func (w *SimpleFileWriter) WriteEntry(e *log.Entry) (n int, err error) {
-	return w.Write(e.Value())
-}
-
-func (w *SimpleFileWriter) Write(p []byte) (n int, err error) {
-	if w.File == nil {
-		return len(p), nil
-	}
-
-	n, err = w.File.Write(p)
-	if err != nil {
-		return
-	}
-
-	w.Written += int64(n)
-	if w.MaxSize > 0 && w.Written > w.MaxSize && w.Filename != "" {
-		err = w.Rotate()
-	}
-
-	return
-}
-
-func (w *SimpleFileWriter) Open() (err error) {
-	err = os.MkdirAll(filepath.Dir(w.Filename), 0755)
-	if err == nil {
-		w.File, err = os.OpenFile(w.Filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err == nil {
-			w.Mut = sync.Mutex{}
-		}
-	}
-
-	return
-}
-
-func (w *SimpleFileWriter) Rotate() (err error) {
-	// rename to new filename
-	now := time.Now()
-	prefix := filepath.Base(w.Filename)
-	name := prefix + "." + now.Format(time.RFC3339) + ".log"
-	os.Rename(w.Filename, name)
-
-	// keep old
-	old := w.File
-
-	f, err := os.OpenFile(w.Filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return
-	}
-	w.File = f
-
-	go func(old *os.File, prefix string) {
-		// change uid/gid
-		uid, _ := strconv.Atoi(os.Getenv("SUDO_UID"))
-		gid, _ := strconv.Atoi(os.Getenv("SUDO_GID"))
-		if uid != 0 && gid != 0 && os.Geteuid() == 0 {
-			// _ = os.Lchown(prefix+".log", uid, gid)
-			_ = os.Chown(prefix+".log", uid, gid)
-		}
-
-		dir := filepath.Dir(w.Filename)
-		dirfile, err := os.Open(dir)
-		if err != nil {
-			return
-		}
-		infos, err := dirfile.Readdir(-1)
-		dirfile.Close()
-		if err != nil {
-			return
-		}
-
-		matches := make([]os.FileInfo, 0)
-		for _, info := range infos {
-			name := info.Name()
-			if strings.HasPrefix(name, prefix) && strings.HasSuffix(name, ".gz") {
-				matches = append(matches, info)
-			}
-		}
-		sort.Slice(matches, func(i, j int) bool {
-			return matches[i].ModTime().Unix() < matches[j].ModTime().Unix()
-		})
-
-		for i := 0; i < len(matches)-w.MaxBackups-1; i++ {
-			os.Remove(filepath.Join(dir, matches[i].Name()))
-		}
-	}(old, prefix)
-
-	return
-}
-
-func NewFileWriter(name string, maxSize int64, maxBackups int) *SimpleFileWriter {
-	w := &SimpleFileWriter{Filename: name, MaxSize: maxSize, MaxBackups: maxBackups}
-
-	if err := w.Open(); err != nil {
-		loggerRoot.Fatal().Msgf("open log failed:%s", err.Error())
-	}
-
-	return w
-}
 
 var LoggerInfo = log.Logger{
-	Level:  log.InfoLevel,
-	Writer: NewFileWriter("log/info.log", 70<<20, 60),
+	Level: log.InfoLevel,
+	Writer: &log.FileWriter{
+		Filename:   "log/info.log",
+		MaxSize:    25 << 20,
+		MaxBackups: 10,
+		ProcessID:  false,
+		HostName:   false,
+	},
 }
 
 var LoggerWarn = log.Logger{
-	Level:  log.WarnLevel,
-	Writer: NewFileWriter("log/warn.log", 70<<20, 60),
+	Level: log.WarnLevel,
+	Writer: &log.FileWriter{
+		Filename:   "log/warn.log",
+		MaxSize:    25 << 20,
+		MaxBackups: 10,
+		ProcessID:  false,
+		HostName:   false,
+	},
 }
 
 var LoggerError = log.Logger{
-	Level:  log.ErrorLevel,
-	Writer: NewFileWriter("log/error.log", 70<<20, 60),
+	Level: log.ErrorLevel,
+	Writer: &log.FileWriter{
+		Filename:   "log/error.log",
+		MaxSize:    25 << 20,
+		MaxBackups: 10,
+		ProcessID:  false,
+		HostName:   false,
+	},
 }
 
 // loggerDebug debug logger
@@ -148,9 +47,11 @@ var loggerDebug = log.Logger{
 	Level: log.DebugLevel,
 	Writer: &log.MultiWriter{
 		InfoWriter: &log.FileWriter{
-			Filename: "log/debug.log", MaxSize: 50 << 20, MaxBackups: 30,
-			ProcessID: false,
-			HostName:  false,
+			Filename:   "log/debug.log",
+			MaxSize:    50 << 20,
+			MaxBackups: 30,
+			ProcessID:  false,
+			HostName:   false,
 		},
 		ConsoleWriter: &log.ConsoleWriter{ColorOutput: false},
 		ConsoleLevel:  log.DebugLevel,
